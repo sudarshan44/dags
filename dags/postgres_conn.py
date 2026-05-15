e7fda009691897caaff5e22c0a2d875ec1eb30e0
sudarshan <60091251+sudarshan44@users.noreply.github.com>
2026-05-15 11:09:06 +0530

    Update postgres_conn.py

--- dags/postgres_conn.py
+++ dags/postgres_conn.py
@@ -1,15 +1,16 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime
 
 with DAG(
     dag_id="postgres_test_dag",
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime(2026, 1, 1),
     schedule=None,
     catchup=False,
 ) as dag:
 
-    test_task = BashOperator(
-        task_id="test_task",
-        bash_command="echo PostgreSQL DAG Loaded Successfully"
+    test_postgres = PostgresOperator(
+        task_id="test_postgres",
+        postgres_conn_id="postgres",
+        sql="select * from budgetary_scheme_expenditure_details LIMIT 5;",
     )
