11c7a523d197432b231119d4ee0aaafa12e7a0d9
sudarshan <60091251+sudarshan44@users.noreply.github.com>
2026-06-10 14:46:16 +0530

    Create minio_conn.py

---
+++ dags/minio_conn.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from datetime import datetime
+
+def test_minio():
+    hook = S3Hook(aws_conn_id="minio_conn")
+
+    client = hook.get_conn()
+
+    response = client.list_buckets()
+
+    print("Buckets:")
+    for bucket in response["Buckets"]:
+        print(bucket["Name"])
+
+with DAG(
+    dag_id="test_minio_connection",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    test = PythonOperator(
+        task_id="test_connection",
+        python_callable=test_minio
+    )
