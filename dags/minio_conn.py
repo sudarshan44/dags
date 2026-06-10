92d79637c4a18be8156a7d1d2f55a6cabe30dcbd
sudarshan <60091251+sudarshan44@users.noreply.github.com>
2026-06-10 14:47:43 +0530

    Update minio_conn.py

--- dags/minio_conn.py
+++ dags/minio_conn.py
@@ -4,7 +4,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime
 
 def test_minio():
-    hook = S3Hook(aws_conn_id="minio_conn")
+    hook = S3Hook(aws_conn_id="minio")
 
     client = hook.get_conn()
 
